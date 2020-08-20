76533cf3f8f379d1cfc386c778078eaabe805747
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-08-21 02:50:08 +0600

    understand compoment

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names = ['Sarwar', 'Mahin', 'Mahdi']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <p>I am a React Person</p>
+        <Person name={names[0]} age="23"></Person>
+        <Person name={names[1]} age="24"></Person>
+        <Person name={names[2]} age="30"></Person>
       </header>
     </div>
   );
 }
 
+function Person(props){
+  
+  return (
+  <div style={{border: '2px solid yellow', margin:'10px', width:'500px'}}>
+    <h1>Name: {props.name}</h1>
+    <p>{props.age} years old</p>
+  </div>
+  );
+}
 export default App;
